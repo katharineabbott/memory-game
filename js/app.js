acadd9d1bf221b4ef7d57266c1820633a30fb605
katharineabbott <katharinemabbott@gmail.com>
2018-07-16 15:14:39 -0700

    added timer to two card limit so that you don't have to click to trigger the event

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 /*
- * Create a list that holds all of your cards
- */
+* Create a list that holds all of your cards
+*/
 
 //let deck = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
 
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+* Display the cards on the page
+*   - shuffle the list of cards using the provided "shuffle" method below
+*   - loop through each card and create its HTML
+*   - add each card's HTML to the page
+*/
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 // function shuffle(array) {
@@ -29,15 +29,15 @@
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+* set up the event listener for a card. If a card is clicked:
+*  - display the card's symbol (put this functionality in another function that you call from this one)
+*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+*  - if the list already has another card, check to see if the two cards match
+*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+*/
 
 let cards = document.querySelectorAll('.card');
 let activeCards = [];
@@ -45,15 +45,23 @@ let activeCards = [];
 document.addEventListener("DOMContentLoaded", function(event) { 
     cards.forEach(function(card) {
         card.addEventListener("click", function(event) {
-            activeCards.push(card);
-           if (activeCards.length > 2) {
-              activeCards.forEach(function(card) {
-                card.classList.remove('open', 'show');
-              });
-              activeCards = [];
-           } else {
-              card.classList.add('open', 'show');
-           }
+        card.classList.add('open', 'show');
+        activeCards.push(card);
+            if (activeCards.length === 2) {
+                setTimeout(function() {
+                    activeCards.forEach(function(card) {
+                        card.classList.remove('open', 'show');
+                    });
+                activeCards = [];
+                }, 300);
+            } 
         });
     });
-  });
+});
+
+function isMatch(card1, card2) {
+    if (card1 === card2) {
+    card.classList.add('match');
+    }
+}
+
